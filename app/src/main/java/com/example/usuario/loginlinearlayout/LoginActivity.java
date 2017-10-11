8fa147146b132b488c953c97ec04f4b0f17f3e7d
mamorky <andres_ball@hotmail.com>
2017-10-11 12:21:20 +0200

    Autor y version

--- app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
+++ app/src/main/java/com/example/usuario/loginlinearlayout/LoginActivity.java
@@ -3,6 +3,11 @@ package com.example.usuario.loginlinearlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author mamorky
+ * @version 1.0
+ * */
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
